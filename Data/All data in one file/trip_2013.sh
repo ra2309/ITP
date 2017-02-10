9a4775bb1738291b385e3b4dd0d0edbc997dabfa
ra2309 <ra2309@nyu.edu>
2017-02-09 23:29:18 -0500

    Add files via upload

---
+++ Data/All data in one file/trip_2013.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+for i in {3..3}
+do
+	for j in {7..9}
+	do
+		wget "https://s3.amazonaws.com/tripdata/201"$i"0"$j"-citibike-tripdata.zip"
+		unzip -p "201"$i"0"$j"-citibike-tripdata.zip" >> data.csv
+		rm "201"$i"0"$j"-citibike-tripdata.zip"
+		#cat "201"$i"-0"$j" - Citi Bike trip data.csv" >> data.csv 
+		#rm "201"$i"-0"$j" - Citi Bike trip data.csv"
+	done
+done
---
+++ Data/Monthly downloading scripts/monthly_OctDec2013.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+for i in {3..3}
+do
+	for j in {10..12}
+	do
+		wget "https://s3.amazonaws.com/tripdata/201"$i$j"-citibike-tripdata.zip"
+		unzip -p "201"$i$j"-citibike-tripdata.zip" >> data2013$j.csv
+		rm "201"$i$j"-citibike-tripdata.zip"
+		#cat "201"$i"-0"$j" - Citi Bike trip data.csv" >> data.csv 
+		#rm "201"$i"-0"$j" - Citi Bike trip data.csv"
+	done
+done
